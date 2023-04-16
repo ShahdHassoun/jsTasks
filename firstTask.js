02bff2142712802f9a71693f71336a1763e946b6
Shahd Hassoun <shahdhassoun2000@gmail.com>
2023-04-16 16:05:07 +0300

    first task

---
+++ firstTask.js
@@ -0,0 +1,43 @@
+// Solve the below questions:
+
+// #1 Turn this array into a new array: [1,2,3,[4],[5]]. Bonus if you can do it on one line
+const array = [[1],[2],[3],[[[4]]],[[[5]]]]
+let array1=array.flat([2]);
+console.log(array1);
+
+// #2 Turn this array into a new array: [ 'Hello young grasshopper!', 'you are', 'learning fast!' ]
+const greeting = [["Hello", "young", "grasshopper!"], ["you", "are"], ["learning", "fast!"]];
+let greeting1=greeting.map(element=>element.join(' '));
+
+console.log(greeting1);
+
+
+//#3 Turn the greeting array above into a string: 'Hello young grasshopper you are learning fast!'
+greeting1=greeting1.join(' ');
+
+
+//#4 Turn the trapped 3 number into: [3]
+const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]];
+let trapped1=trapped.flat([25]);
+console.log(trapped1);
+
+
+//#5 Clean up this email to have no whitespaces. Make the answer be in a single line (return a new string):
+const userEmail3 = '     cannotfillemailformcorrectly@gmail.com   '
+let userEmail=userEmail3.replace(/\s/g, "");
+console.log(userEmail);
+
+
+//#6 Turn the below users (value is their ID number) into an array: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
+const users = { user1: 18273, user2: 92833, user3: 90315 }
+let u= Object.entries(users);
+console.log(u);
+
+//#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
+
+u.map(el=>el[1]=el[1]*2);
+console.log(u);
+
+//#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
+u=Object.fromEntries(u);
+console.log(u);
